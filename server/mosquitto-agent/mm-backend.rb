1c1348bf971f748949f251489f797ba776c79987
Stefano Bocconi <stefano@waag.org>
2016-03-30 15:23:30 +0200

    Adding vagrant and mosquitto related files

---
+++ server/mosquitto-agent/mm-backend.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'mqtt'
+require 'yaml'
+require 'pg'
+
+
+dir = File.dirname(File.expand_path(__FILE__))
+ms_conf = YAML.load_file("#{dir}/makingsense.yaml")
+
+puts ms_conf
+
+client = MQTT::Client.connect(
+  :host => ms_conf['mqtt']['host'],
+  :port => ms_conf['mqtt']['port'],
+  :ssl => ms_conf['mqtt']['ssl'],
+  :clean_session => ms_conf['mqtt']['clean_session'],
+  :client_id => ms_conf['mqtt']['client_id'],
+  :username => ms_conf['mqtt']['username'],
+  :password => ms_conf['mqtt']['password']
+)
+
+client.subscribe([ms_conf['mqtt']['topic'],ms_conf['mqtt']['QoS']])
+
+conn = PGconn.open(
+  :host => ms_conf['db']['host'],
+  :port => ms_conf['db']['port'],
+  :options => ms_conf['db']['options'],
+  :tty =>  ms_conf['db']['tty'],
+  :dbname => ms_conf['db']['dbname'],
+  :user => ms_conf['db']['user'],
+  :password => ms_conf['db']['password']
+)
+
+while true do
+  # Subscribe example
+  topic,measure = client.get()
+  ts = Time.now.strftime('%Y-%m-%d %H:%M:%S.%L%z')
+
+  puts "#{topic} #{measure} #{ts}"
+
+  id = topic.delete(ms_conf['mqtt']['topic'].delete('+'))
+
+  res = conn.exec('INSERT INTO measures (sensor_id, topic, measures, measure_ts) VALUES ($1, $2, $3, $4)',[id,topic, measure, ts])
+
+end
+
+client.disconnect()
+conn.close()
---
+++ server/mosquitto-agent/testMQTT.sh
@@ -0,0 +1,62 @@
+if (( $# != 3 ))
+then
+  echo "Usage: ${0} SENSORS MEASURES RUNS"
+  exit 1;
+fi
+
+SENSORS=${1}
+MEASURES=${2}
+RUNS=${3}
+
+MY_USER=MSPublisher
+MY_PASSWD=ferf345gootkndssdyrwt
+MY_QOS=1
+MY_HOST=wg66.waag.org
+
+function publish {
+MSR=${1}
+for (( d=1; d<=${SENSORS}; d++ ))
+do
+  ID="${d}"
+
+  RND_TXT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  #MY_MSG="{\"measure\":\"${RND_TXT}\",\"id\":${ID}}"
+  MY_MSG="{\"measure\":\"${ID}_${MSR}\",\"id\":${ID}}"
+
+	RESULT="$(/usr/local/bin/mosquitto_pub -h ${MY_HOST} -u ${MY_USER} -P ${MY_PASSWD} -i ${ID} -t sensor/${ID}/data -m $MY_MSG -q ${MY_QOS} 2>&1 ) "
+  #echo _${RESULT}_
+
+	#if ! echo ${RESULT} | gSENSORS "${1}" >/dev/null
+	if [ "${RESULT} " != "  " ]
+	then
+		echo "ERROR: ${RESULT}"
+	else
+    :
+		#echo "DONE"
+	fi
+done
+}
+
+function test {
+
+for (( c=1; c<=${MEASURES}; c++ ))
+do
+	#echo ${c};
+	publish ${c} &
+done
+
+wait
+}
+
+function runTest {
+
+for (( run=1; run<=${RUNS}; run++ ))
+	do
+		echo "Testing ${SENSORS} Sensors each with ${MEASURES} measures, run ${run} of ${RUNS}"
+		time test
+	done
+}
+
+clear
+
+time runTest
