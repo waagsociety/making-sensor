68866b7dff40278614bd2ee3694deae792c097ed
Stefano Bocconi <stefano@waag.org>
2016-03-30 17:53:08 +0200

    Adding retry to the test

--- server/mosquitto-agent/testMQTT.sh
+++ server/mosquitto-agent/testMQTT.sh
@@ -19,21 +19,28 @@ for (( d=1; d<=${SENSORS}; d++ ))
 do
   ID="${d}"
 
-  RND_TXT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  #RND_TXT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   #MY_MSG="{\"measure\":\"${RND_TXT}\",\"id\":${ID}}"
   MY_MSG="{\"measure\":\"${ID}_${MSR}\",\"id\":${ID}}"
 
-	RESULT="$(mosquitto_pub -h ${MY_HOST} -u ${MY_USER} -P ${MY_PASSWD} -i ${ID} -t sensor/${ID}/data -m $MY_MSG -q ${MY_QOS} 2>&1 ) "
-  #echo _${RESULT}_
+  #echo "Sensor nr. ${d}"
 
-	#if ! echo ${RESULT} | gSENSORS "${1}" >/dev/null
-	if [ "${RESULT} " != "  " ]
-	then
-		echo "ERROR: ${RESULT}"
-	else
-    :
-		#echo "DONE"
-	fi
+  while true
+  do
+	   RESULT="$(mosquitto_pub -h ${MY_HOST} -u ${MY_USER} -P ${MY_PASSWD} -i ${ID} -t sensor/${ID}/data -m $MY_MSG -q ${MY_QOS} 2>&1 ) "
+     #echo _${RESULT}_
+
+	  #if ! echo ${RESULT} | gSENSORS "${1}" >/dev/null
+	  if [ "${RESULT} " != "  " ]
+	  then
+		  echo "ERROR: ${RESULT}, retrying"
+	  else
+      break
+		  #echo "DONE"
+	  fi
+  done
+  RND_SLEEP="0.$[ 1 + $[ RANDOM % 9 ]]"
+  sleep ${RND_SLEEP}
 done
 }
 
@@ -42,7 +49,9 @@ function test {
 for (( c=1; c<=${MEASURES}; c++ ))
 do
 	#echo ${c};
+  echo "Measure nr. ${c}"
 	publish ${c} &
+  sleep 1
 done
 
 wait
