b650b481d1c49bb8aab2fdd2529a07512513f81a
Stefano Bocconi <stefano@waag.org>
2016-04-26 14:23:10 +0200

    Adding AWS deployment

--- server/puphpet/files/exec-once-unprivileged/installCitySDKSource.sh
+++ server/puphpet/files/exec-once-unprivileged/installCitySDKSource.sh
@@ -3,6 +3,13 @@ echo "Checking out citysdk-amsterdam in ${MY_DIR}"
 
 export DEBIAN_FRONTEND=noninteractive
 
+if [ ! -d ${MY_DIR} ]
+then
+  mkdir -p ${MY_DIR}
+else
+  echo "${MY_DIR} already exists"
+fi
+
 cd ${MY_DIR}
 
 if [ ! -d citysdk-amsterdam ]
---
+++ server/setVagrantAWS.sh
@@ -0,0 +1,37 @@
+if [ "$(echo $0)" != "-bash" ]
+then
+	echo "Run the command with source!!!"
+	exit 1
+fi
+
+MY_CMD=$1
+
+if [ -z "${MY_CMD}" ]
+then
+	echo "You must specify either ON or OFF, exiting"
+	return 1
+fi
+
+
+if [ "${MY_CMD}" = "ON" ]
+then
+  echo "Setting Vagrant for AWS"
+  if ! vagrant plugin list | grep vagrant-aws
+  then
+    vagrant plugin install vagrant-aws
+  fi
+
+  if ! vagrant box list | grep dummy | grep aws
+  then
+    vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+  fi
+
+  export VAGRANT_DEFAULT_PROVIDER=aws && echo VAGRANT_DEFAULT_PROVIDER=$VAGRANT_DEFAULT_PROVIDER!!
+  export AWS_ACCESS_KEY_ID=$(cat ~/.aws/credentials | grep -i AWS_ACCESS_KEY_ID | tr -d ' ' | cut -d'=' -f2) && echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID!!
+  export AWS_SECRET_ACCESS_KEY=$(cat ~/.aws/credentials | grep -i AWS_SECRET_ACCESS_KEY | tr -d ' ' | cut -d'=' -f2) && echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY!!
+else
+  echo "Resetting Vagrant"
+  unset VAGRANT_DEFAULT_PROVIDER
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+fi
