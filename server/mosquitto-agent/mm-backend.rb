2e542eebb0c7c21e7bf6d121d0ab0f84dba4e60a
Stefano Bocconi <stefano@waag.org>
2016-04-29 13:50:12 +0200

    Handle SIGTERM called on program and exit

--- server/mosquitto-agent/mm-backend.rb
+++ server/mosquitto-agent/mm-backend.rb
@@ -10,6 +10,12 @@ $mqtt_client = nil
 $db_conn = nil
 $byebye = false
 
+# Signal handler to handle a clean exit
+Signal.trap("SIGTERM") {
+  puts "Exiting"
+  $byebye = true
+}
+
 # Release the MQTT connection
 def closeMQTTConn(client)
 
@@ -33,29 +39,32 @@ def makeMQTTConnection(conf, client)
   # trying anyway to release the client just in case
   closeMQTTConn(client)
 
-  is_on = false
+  begin
+    client = MQTT::Client.connect(
+      :host => conf['mqtt']['host'],
+      :port => conf['mqtt']['port'],
+      :ssl => conf['mqtt']['ssl'],
+      :clean_session => conf['mqtt']['clean_session'],
+      :client_id => conf['mqtt']['client_id'],
+      :username => conf['mqtt']['username'],
+      :password => conf['mqtt']['password']
+    )
+    client.subscribe([conf['mqtt']['topic'],conf['mqtt']['QoS']])
 
-  while ! is_on do
-    begin
-      client = MQTT::Client.connect(
-        :host => conf['mqtt']['host'],
-        :port => conf['mqtt']['port'],
-        :ssl => conf['mqtt']['ssl'],
-        :clean_session => conf['mqtt']['clean_session'],
-        :client_id => conf['mqtt']['client_id'],
-        :username => conf['mqtt']['username'],
-        :password => conf['mqtt']['password']
-      )
-      client.subscribe([conf['mqtt']['topic'],conf['mqtt']['QoS']])
+  rescue MQTT::Exception,Errno::ECONNREFUSED,Errno::ENETUNREACH,SocketError => e
+    $stderr.puts "Error in connecting to MQTT server, class: #{e.class.name}, message: #{e.message}"
 
-      is_on = true
-
-    rescue MQTT::Exception,Errno::ECONNREFUSED,Errno::ENETUNREACH,SocketError => e
-      $stderr.puts "Error in connecting to MQTT server, class: #{e.class.name}, message: #{e.message}, sleep and retry"
-      sleep conf['mqtt']['retry']
+    if $byebye
+      return nil
     end
+
+    $stderr.puts "Sleep and retry"
+    sleep conf['mqtt']['retry']
+    retry
   end
+
   return client
+
 end
 
 # Release the DB connection
@@ -79,31 +88,32 @@ def makeDBConnection(conf, conn)
   # trying anyway to release the connection just in case
   closeDBConn(conn)
 
-  is_on = false
+  begin
+    conn = PGconn.open(
+      :host => conf['airqdb']['host'],
+      :port => conf['airqdb']['port'],
+      :options => conf['airqdb']['options'],
+      :tty =>  conf['airqdb']['tty'],
+      :dbname => conf['airqdb']['dbname'],
+      :user => conf['airqdb']['user'],
+      :password => conf['airqdb']['password']
+    )
 
-  while ! is_on do
-    begin
+    conn.prepare("sensordata", "INSERT INTO #{conf['airqdb']['measurestable']} (id, srv_ts, topic, rssi, temp, pm10, pm25, no2a, no2b, message) " +
+                "VALUES ($1::bigint, $2::timestamp with time zone, $3::text, $4::smallint, $5::numeric, $6::numeric, $7::numeric, $8::numeric, $9::numeric, $10::text)")
 
-      conn = PGconn.open(
-        :host => conf['airqdb']['host'],
-        :port => conf['airqdb']['port'],
-        :options => conf['airqdb']['options'],
-        :tty =>  conf['airqdb']['tty'],
-        :dbname => conf['airqdb']['dbname'],
-        :user => conf['airqdb']['user'],
-        :password => conf['airqdb']['password']
-      )
+  rescue PGError => e
+    $stderr.puts "Error in connecting to Postgres server, class: #{e.class.name}, message: #{e.message}"
 
-      conn.prepare("sensordata", "INSERT INTO #{conf['airqdb']['measurestable']} (id, srv_ts, topic, rssi, temp, pm10, pm25, no2a, no2b, message) " +
-                  "VALUES ($1::bigint, $2::timestamp with time zone, $3::text, $4::smallint, $5::numeric, $6::numeric, $7::numeric, $8::numeric, $9::numeric, $10::text)")
-
-      is_on = true
-
-    rescue PGError => e
-      $stderr.puts "Error in connecting to Postgres server, class: #{e.class.name}, message: #{e.message}, sleep and retry"
-      sleep conf['airqdb']['retry']
+    if $byebye
+      return nil
     end
+
+    $stderr.puts "Sleep and retry"
+    sleep conf['airqdb']['retry']
+    retry
   end
+
   return conn
 end
 
@@ -114,11 +124,6 @@ def clean_up()
   closeDBConn($db_conn)
 end
 
-# Signal handler to handle a clean exit
-Signal.trap("SIGTERM") {
-  puts "Exiting"
-  $byebye = true
-  }
 
 # here the main part starts
 dir = File.dirname(File.expand_path(__FILE__))
@@ -132,19 +137,22 @@ db_conn = makeDBConnection(ms_conf,nil)
 
 while ! $byebye do
   begin
-    done = false
-    while ! done
-      begin
-        # blocking call, not ideal if need to exit
-        topic,msg = mqtt_client.get()
-        done = true
-      rescue MQTT::Exception => e
-        $stderr.puts "Error with the MQTT connection, class: #{e.class.name}, message: #{e.message}"
-        $stderr.puts "Sleep and retry"
-        sleep ms_conf['mqtt']['retry']
-        mqtt_client = makeMQTTConnection(ms_conf,mqtt_client)
+    begin
+      # blocking call, not ideal if need to exit
+      topic,msg = mqtt_client.get()
+    rescue MQTT::Exception => e
+      $stderr.puts "Error with the MQTT connection, class: #{e.class.name}, message: #{e.message}"
+
+      if $byebye
+        break
       end
+
+      $stderr.puts "Sleep and retry"
+      sleep ms_conf['mqtt']['retry']
+      mqtt_client = makeMQTTConnection(ms_conf,mqtt_client)
+      retry
     end
+
 
     srv_ts = Time.now.strftime('%Y-%m-%d %H:%M:%S.%L%z')
 
@@ -154,35 +162,44 @@ while ! $byebye do
       msg_hash = JSON.parse(msg,symbolize_names: true)
     rescue JSON::JSONError => e
       $stderr.puts "Error processing sensor data: #{msg}, class: #{e.class.name}, message: #{e.message}"
-      $stderr.puts "Skip message and continue"
-      next
+      $stderr.puts "Save raw message with fake id"
+      msg_hash[:id] = -1
+      msg_hash[:message] = msg
     end
 
     puts "topic: #{topic}, msg: #{msg}, timestamp: #{srv_ts}, hash: " + msg_hash.to_s
 
     #id = topic.delete(ms_conf['mqtt']['topic'].delete('+'))
 
-    done = false
-    while ! done
-      begin
-        res = db_conn.exec_prepared("sensordata",[msg_hash[:id], srv_ts, topic, msg_hash[:rssi], msg_hash[:temp], msg_hash[:pm10],
-              msg_hash["pm2.5".to_sym], msg_hash[:no2a], msg_hash[:no2b], msg_hash[:message]])
-        done = true
-      rescue PG::NotNullViolation => e
-        $stderr.puts "Error inserting message: #{msg}, error: #{e.message}"
-        $stderr.puts "Ignore message"
-        done = true
-      rescue PGError => e
-        $stderr.puts "Error with the DB connection, class: #{e.class.name}, message: #{e.message}"
-        $stderr.puts "Sleep and retry"
-        sleep ms_conf['airqdb']['retry']
-        db_conn = makeDBConnection(ms_conf,db_conn)
+    begin
+      res = db_conn.exec_prepared("sensordata",[msg_hash[:id], srv_ts, topic, msg_hash[:rssi], msg_hash[:temp], msg_hash[:pm10],
+            msg_hash["pm2.5".to_sym], msg_hash[:no2a], msg_hash[:no2b], msg_hash[:message]])
+    rescue PG::NotNullViolation => e
+      $stderr.puts "Error inserting message: #{msg}, error: #{e.message}"
+      $stderr.puts "Ignore message"
+    rescue PGError => e
+      $stderr.puts "Error with the DB connection, class: #{e.class.name}, message: #{e.message}"
+
+      if $byebye
+        break
       end
+
+      $stderr.puts "Sleep and retry"
+      sleep ms_conf['airqdb']['retry']
+      db_conn = makeDBConnection(ms_conf,db_conn)
+      retry
+
     end
+
 
   rescue Exception => e
     $stderr.puts "Error in process loop, class: #{e.class.name}, message: #{e.message}"
     $stderr.puts "Sensor data: topic: #{topic}, msg: #{msg}, timestamp: #{srv_ts}, hash: #{msg_hash.to_s}"
+
+    if $byebye
+      break
+    end
+
     $stderr.puts "Sleep and continue"
     sleep ms_conf['mqtt']['retry']
   end
