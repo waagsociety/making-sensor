0e4bacbbff3cd4f01149b68ec7942b65adc31243
Stefano Bocconi <stefano@waag.org>
2016-05-10 21:05:32 +0200

    files for the sensor kit

---
+++ sensor/datalogger_Emma_dht.ino
@@ -0,0 +1,286 @@
+
+// Code for Arduino Uno
+// No2, temperature & Finedust sensor
+// Waag Society, Making Sense 
+// author: Dave Gonner & Emma Pareschi
+// version 4 May 2016
+
+#include <Wire.h>u
+#include <Adafruit_ADS1015.h>
+#include <OneWire.h>
+//#include <DallasTemperature.h>
+#include "DHT.h"
+
+#define ONE_WIRE_BUS 2      // Data wire is plugged into pin 2 on the Arduino
+#define LOG_INTERVAL  925  // mills between entries (reduce to take more/faster data)
+
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+//OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+//DallasTemperature tempSensor(&oneWire);
+
+#define DHTPIN A1
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// VOOR DE ADC
+
+// Averagigng period:
+int  FLAVG = 32;  // 60
+
+Adafruit_ADS1115 ads1115;
+
+float Vstep = 5.0 / 32768.0 * 1000.0;  // Assuming 16 bits adc, units is mV
+int  iptr = 0;                          // iptr is the pointer to the table holding the second-values
+// and can be used as the number of seconds in the minute.
+
+int16_t results1;
+int16_t results2;
+//uint32_t results1;
+//uint32_t results2;
+
+uint32_t acc_op1 = 0;                       // accumulator 1 value
+uint32_t acc_op2 = 0;                       // accumulator 2 value
+
+
+float pm10 = 0.0;
+float pm2_5 = 0.0;
+
+
+//---------------------------
+// Dust Sensor
+
+//DustDuino Serial
+//By Matthew Schroyer, MentalMunition.com
+//Created 2/19/2014
+//Last updated 8/24/2015
+
+// DESCRIPTION
+// Outputs particle concentration per cubic foot and
+// mass concentration (microgram per cubic meter) to
+// serial, from a Shinyei particuate matter sensor.
+
+// CONNECTING THE DUSTDUINO
+// P1 channel on sensor is connected to digital 3
+// P2 channel on sensor is connected to digital 2
+
+// THEORY OF OPERATION
+// Sketch measures the width of pulses through
+// boolean triggers, on each channel.
+// Pulse width is converted into a percent integer
+// of time on, and equation uses this to determine
+// particle concentration.
+// Shape, size, and density are assumed for PM10
+// and PM2.5 particles. Mass concentration is
+// estimated based on these assumptions, along with
+// the particle concentration.
+
+
+
+unsigned long starttime;
+
+unsigned long triggerOnP1;
+unsigned long triggerOffP1;
+unsigned long pulseLengthP1;
+unsigned long durationP1;
+boolean valP1 = HIGH;
+boolean triggerP1 = false;
+
+unsigned long triggerOnP2;
+unsigned long triggerOffP2;
+unsigned long pulseLengthP2;
+unsigned long durationP2;
+boolean valP2 = HIGH;
+boolean triggerP2 = false;
+
+float ratioP1 = 0;
+float ratioP2 = 0;
+unsigned long sampletime_ms = 30000;
+float countP1;
+float countP2;
+
+#define DUST_P1_PIN 4      // Dust sensor PM2.5 pin
+#define DUST_P2_PIN 3      // Dust senspr PM10 pin
+
+
+//void error(char *str) {
+//
+//  Serial.println("========================================");
+//  Serial.print("error: ");
+//  Serial.println(str);
+//  Serial.println("========================================");
+//  while (1);
+//}
+
+void setup(void) {
+
+  pinMode(DHTPIN, INPUT);
+
+  pinMode(DUST_P1_PIN, INPUT); 
+  pinMode(DUST_P2_PIN, INPUT); 
+
+  int i;
+  Serial.begin(9600);
+  Serial.println();
+
+  // Start up the temperature sensor library
+  dht.begin();
+
+  // VOOR DE ASCL
+  // If you want to set the aref to something other than 5v
+  analogReference(EXTERNAL);
+
+  Serial.println("Making Sense, Waag Society Amsterdam");
+  Serial.println("- Alphasense NO2-B42F sensor");
+  Serial.println("- DHT22 temp sensor");
+  Serial.println("- Shinyei PPD42 PM10 and PM2.5 sensor");
+  Serial.println("Version 25 april 2016");
+
+  ads1115.begin();
+}
+
+
+void loop(void) {
+  // first do 30 seconds of dust measurement
+  loop_dustsensor();
+  // then do 30 seconds of no2 measurement
+  loop_no2();
+
+}
+
+
+void loop_no2(void) {
+//  Serial.println("Start NO2 measurments...");
+    iptr=0;
+  
+    while (iptr < FLAVG) {
+    results1 = ads1115.readADC_Differential_0_1();    // Read ADC ports 0 and 1    
+    results2 = ads1115.readADC_Differential_2_3();    // Read ADC ports 2 and 3
+    
+    // SEND EVERYTHING TO THE ESP8266
+//    Serial.print("--RAW:");
+//    Serial.print(results1);
+//    Serial.print(":");
+//    Serial.print(results2);
+//    Serial.print(":");
+//    Serial.print(iptr);
+//    Serial.println("#");
+
+    acc_op1 += results1;
+    acc_op2 += results2;
+
+    iptr++;
+
+    // delay for the amount of time we want between readings
+    delay(LOG_INTERVAL);
+  }
+
+//    Serial.print("acc_op1");
+//    Serial.println(acc_op1);
+
+//    Serial.print("acc_op2");
+//    Serial.println(acc_op2);
+
+//  if (iptr > FLAVG - 1) {
+
+    //-------------------------------------------  
+
+    float temperature = dht.readTemperature();
+    float hum = dht.readHumidity();
+//    float no2_a = avg_op1 / FLAVG;
+//    float no2_b = avg_op2 / FLAVG;
+
+    int no2_a = 0;
+    int no2_b = 0;
+
+    if (FLAVG == 32) {
+      no2_a = acc_op1 >> 5; // right shift by 5 equals divide by 32
+      no2_b = acc_op2 >> 5; // right shift by 5 equals divide by 32
+    }
+    
+    if (FLAVG == 64) {
+      no2_a = acc_op1 >> 6; // right shift by 6 equals divide by 64
+      no2_b = acc_op2 >> 6; // right shift by 6 equals divide by 64
+    }
+     
+    // SEND EVERYTHING TO THE ESP8266
+    Serial.println();
+    Serial.print("--ESP:");
+    Serial.print(temperature);
+    Serial.print(":");
+    Serial.print(hum);
+    Serial.print(":");
+    Serial.print(no2_a);
+    Serial.print(":");
+    Serial.print(no2_b);
+    Serial.print(":");
+    Serial.print(pm10);
+    Serial.print(":");
+    Serial.print(pm2_5);
+    Serial.println("#");
+
+    iptr = 0;
+    acc_op1 = 0;
+    acc_op2 = 0;
+    pm10 = 0.0;
+    pm2_5 = 0.0;
+}
+
+
+void loop_dustsensor() {
+
+//     Serial.println("Start Dust measurments...");
+     
+  starttime = millis();
+  durationP1 = 0;
+  durationP2 = 0;
+
+  // take samples for 30 seconds
+  while ((millis() - starttime) < sampletime_ms) {
+ 
+    valP1 = digitalRead(DUST_P1_PIN); // 3
+    valP2 = digitalRead(DUST_P2_PIN);  // 2
+  
+    if (valP1 == LOW && triggerP1 == false) {
+      triggerP1 = true;
+      triggerOnP1 = micros();
+    }
+  
+    if (valP1 == HIGH && triggerP1 == true) {
+      triggerOffP1 = micros();
+      pulseLengthP1 = triggerOffP1 - triggerOnP1;
+      durationP1 = durationP1 + pulseLengthP1;
+      triggerP1 = false;
+    }
+  
+    if (valP2 == LOW && triggerP2 == false) {
+      triggerP2 = true;
+      triggerOnP2 = micros();
+    }
+  
+    if (valP2 == HIGH && triggerP2 == true){
+      triggerOffP2 = micros();
+      pulseLengthP2 = triggerOffP2 - triggerOnP2;
+      durationP2 = durationP2 + pulseLengthP2;
+      triggerP2 = false;
+    }
+  }
+
+  // sampling is over, calculate results
+      
+  ratioP1 = durationP1/(sampletime_ms*10.0);  // Integer percentage 0=>100
+  ratioP2 = durationP2/(sampletime_ms*10.0);
+  countP1 = 1.1*pow(ratioP1,3)-3.8*pow(ratioP1,2)+520*ratioP1+0.62;
+  countP2 = 1.1*pow(ratioP2,3)-3.8*pow(ratioP2,2)+520*ratioP2+0.62;
+  float PM10count = countP2;
+  float PM25count = countP1 - countP2;
+
+  pm10 = PM10count;
+  pm2_5 = PM25count;
+
+    
+}
+
---
+++ sensor/making-sense-esp8266-dht.ino
@@ -0,0 +1,335 @@
+
+// Code for NodeMCU v1.0 module (ESP8266)
+// Waag Society, Making Sense
+// author: Dave Gonner & Emma Pareschi
+// version 11 May 2016
+
+
+#include <FS.h>                   // Make sure ESP library 2.1.0 or higher is installed
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include <PubSubClient.h>         //https://github.com/knolleary/pubsubclient
+
+// DEFAULT MQTT SETTINGS, will be overwritten by values from config.json
+//char mqtt_server[41] = "m21.cloudmqtt.com\0";
+//char mqtt_portStr[7] = "12132\0";
+//char mqtt_username[21] = "no2_sensor\0";
+//char mqtt_password[21] = "WaagSociety\0";
+//char mqtt_topic[21] = "sensor/%u/data\0";
+
+//char mqtt_server[41] = "wg66.waag.org\0";
+char mqtt_server[41] = "<my_server>\0";
+char mqtt_portStr[7] = "<my_port>\0";
+char mqtt_username[21] = "<my_username>\0";
+char mqtt_password[21] = "<my_password>\0";
+char mqtt_topic[21] = "<my_topic>\0";
+
+int mqtt_port = atoi(mqtt_portStr);
+
+#define BUTTON_PIN 16              // D0, button to enter wifi manager
+#define RX_ACTIVITY_LED_PIN 5      // D1
+#define MQTT_CONNECTED_LED_PIN 4   // D2
+#define WIFI_CONNECTED_LED_PIN 14  // D5
+#define WIFI_CONFIG_LED_PIN 12     // D6
+
+
+WiFiManager wifiManager;
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+String readStr;
+long chipid;
+bool shouldSaveConfig = false;    //flag for saving data
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void saveConfigJson() {
+  //save the custom parameters to FS
+  Serial.println("saving config");
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["mqtt_server"] = mqtt_server;
+  json["mqtt_port"] = mqtt_portStr;
+  json["mqtt_username"] = mqtt_username;
+  json["mqtt_password"] = mqtt_password;
+  json["mqtt_topic"] = mqtt_topic;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("failed to open config file for writing");
+  }
+
+  json.printTo(Serial);
+  Serial.println();
+  json.printTo(configFile);
+  configFile.close();
+  //end save
+}
+
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println();
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(WIFI_CONFIG_LED_PIN, OUTPUT);
+  digitalWrite(WIFI_CONFIG_LED_PIN, HIGH); // off
+  pinMode(WIFI_CONNECTED_LED_PIN, OUTPUT);
+  digitalWrite(WIFI_CONNECTED_LED_PIN, HIGH); // off
+  pinMode(MQTT_CONNECTED_LED_PIN, OUTPUT);
+  digitalWrite(MQTT_CONNECTED_LED_PIN, HIGH); // off
+  pinMode(RX_ACTIVITY_LED_PIN, OUTPUT);
+  digitalWrite(RX_ACTIVITY_LED_PIN, HIGH); // off
+
+
+  //Format FS, reset Wifi settings, for testing
+//  Serial.print("Formatting FS...");
+//  SPIFFS.format();
+//  Serial.println("Done.");
+//  Serial.print("Reset WiFi settings...");
+//   wifiManager.resetSettings();  //****
+//  Serial.println("Done.");
+//  while(1) {
+//    delay(1000);
+//    Serial.println("loop...");
+//  }
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_portStr, json["mqtt_port"]);
+          mqtt_port = atoi(mqtt_portStr);
+          strcpy(mqtt_username, json["mqtt_username"]);
+          strcpy(mqtt_password, json["mqtt_password"]);
+          strcpy(mqtt_topic, json["mqtt_topic"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    } else {
+      Serial.println("/config.json does not exist, creating");
+      saveConfigJson(); // saving the hardcoded default values
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  boolean startConfigPortal = false;
+  if ( digitalRead(BUTTON_PIN) == LOW ) {
+    startConfigPortal = true;
+  }
+
+  WiFi.mode(WIFI_STA);
+  if (WiFi.SSID()) {
+    Serial.println("Using saved credentials");
+    ETS_UART_INTR_DISABLE();
+    wifi_station_disconnect();
+    ETS_UART_INTR_ENABLE();
+    WiFi.begin();
+  } else {
+    Serial.println("No saved credentials");
+    startConfigPortal = true;
+  }
+
+  WiFi.waitForConnectResult();
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Failed to connect Wifi");
+    startConfigPortal = true;
+  }
+
+  if (startConfigPortal) {
+    WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+    WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_portStr, 6);
+    WiFiManagerParameter custom_mqtt_username("username", "mqtt username", mqtt_username, 20);
+    WiFiManagerParameter custom_mqtt_password("password", "mqtt password", mqtt_password, 20);
+    WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 20);
+
+    wifiManager.addParameter(&custom_mqtt_server);
+    wifiManager.addParameter(&custom_mqtt_port);
+    wifiManager.addParameter(&custom_mqtt_username);
+    wifiManager.addParameter(&custom_mqtt_password);
+    wifiManager.addParameter(&custom_mqtt_topic);
+
+    // If the user requests it, start the wifimanager
+    digitalWrite(WIFI_CONNECTED_LED_PIN, HIGH); // off
+    digitalWrite(MQTT_CONNECTED_LED_PIN, HIGH); // off
+    digitalWrite(WIFI_CONFIG_LED_PIN, LOW); // on
+    wifiManager.startConfigPortal("MakingSense");
+    digitalWrite(WIFI_CONFIG_LED_PIN, HIGH); // off
+
+    if (shouldSaveConfig) {
+      // read the updated parameters
+      strcpy(mqtt_server, custom_mqtt_server.getValue());
+      strcpy(mqtt_portStr, custom_mqtt_port.getValue());
+      mqtt_port = atoi(mqtt_portStr);
+      strcpy(mqtt_username, custom_mqtt_username.getValue());
+      strcpy(mqtt_password, custom_mqtt_password.getValue());
+      strcpy(mqtt_topic, custom_mqtt_topic.getValue());
+
+      saveConfigJson();
+      shouldSaveConfig = false;
+    }
+  }
+
+  digitalWrite(WIFI_CONNECTED_LED_PIN, LOW); // on
+  Serial.println("Wifi connected...");
+
+  mqttClient.setServer(mqtt_server, mqtt_port);
+  chipid = ESP.getChipId();
+}
+
+
+void reconnect() {
+  digitalWrite(MQTT_CONNECTED_LED_PIN, HIGH); // off
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection to ");
+    Serial.print(mqtt_server);
+    Serial.println("...");
+    // Attempt to connect
+    char mqtt_clientid[15];
+    snprintf (mqtt_clientid, 14, "ESP%u", chipid);
+
+    if (mqttClient.connect(mqtt_clientid, mqtt_username, mqtt_password)) {
+      digitalWrite(WIFI_CONNECTED_LED_PIN, LOW); // on
+      digitalWrite(MQTT_CONNECTED_LED_PIN, LOW); // on
+      Serial.println("MQTT connected.");
+      long rssi = WiFi.RSSI();
+
+ //     char buf[50];
+ //     sprintf(buf, "ESP: %u Connected @ %i dBm", chipid, rssi);
+ //     char topic_buf[50];
+ //     sprintf(topic_buf, mqtt_topic, chipid);
+ //     mqttClient.publish(topic_buf, buf);
+
+      // send proper JSON startup message
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& json = jsonBuffer.createObject();
+      json["id"] = chipid;
+      json["rssi"] = rssi;
+      json["message"] = "Sensor startup";
+      char buf[110];
+      json.printTo(buf, sizeof(buf));
+
+      Serial.print("Publish message: ");
+      Serial.println(buf);
+
+      char topic_buf[50];
+      sprintf(topic_buf, mqtt_topic, chipid);
+      mqttClient.publish(topic_buf, buf);
+
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void loop() {
+
+  // make sure the MQTT client is connencted
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop(); // Need to call this, otherwise mqtt breaks
+
+  // read data from the serial port
+  while (Serial.available()) {
+    digitalWrite(RX_ACTIVITY_LED_PIN, LOW); // on
+    delay(3);  //delay to allow buffer to fill
+    if (Serial.available() >0) {
+      char c = Serial.read();  //gets one byte from serial buffer
+      // if the byte is not end of line, append it
+      if (c == 10) { // end of the line
+        if (readStr.startsWith("--ESP:") && readStr.endsWith("#\r")) { // test to see for esp command line
+          int colom1 = readStr.indexOf(':',7);
+          int colom2 = readStr.indexOf(':',colom1+1);
+          int colom3 = readStr.indexOf(':',colom2+1);
+          int colom4 = readStr.indexOf(':',colom3+1);
+          int colom5 = readStr.indexOf(':',colom4+1);
+          int colom6 = readStr.indexOf(':',colom5+1);
+
+          String temp = readStr.substring(6, colom1);
+          String hum = readStr.substring(colom1+1, colom2);
+          String no2_a = readStr.substring(colom2+1, colom3);
+          String no2_b = readStr.substring(colom3+1, colom4);
+          String pm10 = readStr.substring(colom4+1, colom5);
+          String pm2_5 = readStr.substring(colom5+1, readStr.length()-2);
+
+          long rssi = WiFi.RSSI(); // RSSI = wifi signal strength
+
+          DynamicJsonBuffer jsonBuffer;
+          JsonObject& json = jsonBuffer.createObject();
+          json["i"] = chipid;
+          json["r"] = rssi;
+          json["t"] = temp;
+          json["a"] = no2_a;
+          json["b"] = no2_b;
+          json["p10"] = pm10;
+          json["p2.5"] = pm2_5;
+          json["h"] = hum;
+
+          char buffer[130];
+          json.printTo(buffer, sizeof(buffer));
+
+          Serial.print("Publish message 1: ");
+          Serial.println(buffer);
+
+          char topic_buf[50];
+          sprintf(topic_buf, mqtt_topic, chipid);
+          int aux = mqttClient.publish(topic_buf, buffer);
+
+          Serial.print("Return from publish: ");
+          Serial.println(aux);
+          Serial.print("topic buff: ");
+          Serial.println(topic_buf);
+
+        }
+
+        Serial.println(readStr);
+        readStr = "";
+      } else {
+        readStr += c; //makes the string readString
+      }
+    }
+  }
+  digitalWrite(RX_ACTIVITY_LED_PIN, HIGH); // off
+
+}
