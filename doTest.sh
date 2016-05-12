01fe2cb64c9627f4df64a1b88cf0632bea343b92
Stefano Bocconi <stefano@waag.org>
2016-05-12 18:06:03 +0200

    More test definition

--- doTest.sh
+++ doTest.sh
@@ -40,72 +40,115 @@ echo "Test start" | tee ${TMP_FILE}
 
 PASSED=true
 
-CONF_FILE=$(find ./ -name makingsense.yaml)
-#echo ${CONF_FILE}
-MY_KEY=$(find . -name airq_key)
-SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-
-if which mosquitto_pub >/dev/null
+if ! curl --max-time 15 ${MY_HOST} &> /dev/null
 then
-  ## Test mosquitto agent
-  MOSQUITTO_USER=$(parse_yaml ${CONF_FILE} mqtt username)
-  MOSQUITTO_PASSWD=$(parse_yaml ${CONF_FILE} mqtt password)
+  echo "ERROR no connection to default webserver on ${MY_HOST}" | tee -a ${TMP_FILE}
+  PASSED=false
+else
 
-  #echo ${MY_USR} ${MY_PWD}
-  TESTID=0
-  MY_QOS=0
-  UNIQUE_MSG="$RANDOM $(date +'%Y-%m-%d %H:%M:%S%z')"
-  MY_MSG="{\"i\":${TESTID},\"rssi\":-75,\"message\":\"${UNIQUE_MSG}\"}"
+  CONF_FILE=$(find ./ -name makingsense.yaml)
+  #echo ${CONF_FILE}
+  MY_KEY=$(find . -name airq_key)
+  SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-  if ! mosquitto_pub -h ${MY_HOST} -u "${MOSQUITTO_USER}" -P "${MOSQUITTO_PASSWD}" -i "${TESTID}" -t "sensor/${ID}/data" -m "$MY_MSG" -q ${MY_QOS}
+  if which mosquitto_pub >/dev/null
   then
-    echo "ERROR mosquitto not set" | tee -a ${TMP_FILE}
+    ## Test mosquitto agent
+    MOSQUITTO_USER=$(parse_yaml ${CONF_FILE} mqtt username)
+    MOSQUITTO_PASSWD=$(parse_yaml ${CONF_FILE} mqtt password)
+
+    #echo ${MY_USR} ${MY_PWD}
+    TESTID=0
+    MY_QOS=0
+    UNIQUE_MSG="$RANDOM $(date +'%Y-%m-%d %H:%M:%S%z')"
+    MY_MSG="{\"i\":${TESTID},\"rssi\":-75,\"message\":\"${UNIQUE_MSG}\"}"
+
+    if ! mosquitto_pub -h ${MY_HOST} -u "${MOSQUITTO_USER}" -P "${MOSQUITTO_PASSWD}" -i "${TESTID}" -t "sensor/${ID}/data" -m "$MY_MSG" -q ${MY_QOS}
+    then
+      echo "ERROR mosquitto not set" | tee -a ${TMP_FILE}
+      PASSED=false
+    fi
+
+  #  echo "Tailing mosquitto log"
+  #  LINES=$((${REP} * ${SENSORS} * 6))
+  #  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} "tail -n ${LINES} /var/log/mosquitto/mosquitto.log"
+
+    AIRQ_USER=$(parse_yaml ${CONF_FILE} airqdb user)
+    AIRQ_PASSWD=$(parse_yaml ${CONF_FILE} airqdb password)
+
+    HITS=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} "PGPASSWORD=${AIRQ_PASSWD} psql -t -A -U ${AIRQ_USER} -h localhost airq -c \"SELECT count(id) from measures WHERE id=${TESTID} AND message = '${UNIQUE_MSG}'\" " 2>/dev/null)
+    if [ "${HITS}" = "0" ]
+    then
+      echo "Something wrong in data acquisition from mosquitto server to DB" | tee -a ${TMP_FILE}
+      PASSED=false
+    else
+      echo "Data acquisition from mosquitto server to DB is OK" | tee -a ${TMP_FILE}
+    fi
+  else
+    echo "mosquitto_pub is not installed, skipping mosquitto test" | tee -a ${TMP_FILE}
     PASSED=false
   fi
 
-#  echo "Tailing mosquitto log"
-#  LINES=$((${REP} * ${SENSORS} * 6))
-#  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} "tail -n ${LINES} /var/log/mosquitto/mosquitto.log"
 
-  AIRQ_USER=$(parse_yaml ${CONF_FILE} airqdb user)
-  AIRQ_PASSWD=$(parse_yaml ${CONF_FILE} airqdb password)
+  ## Test traffic data
 
-  HITS=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} "PGPASSWORD=${AIRQ_PASSWD} psql -t -A -U ${AIRQ_USER} -h localhost airq -c \"SELECT count(id) from measures WHERE id=${TESTID} AND message = '${UNIQUE_MSG}'\" " 2>/dev/null)
-  if [ "${HITS}" = "0" ]
+  MY_TIME=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} 'sudo su postgres -c "psql -t -A -d traffic -c \"SELECT max(timestmp) from traveltime\" " ' 2>/dev/null)
+  echo "Most recent traffic data: ${MY_TIME}" | tee -a ${TMP_FILE}
+  diff_min "${MY_TIME}"
+  echo "Data is ${ELAPSED_MIN} min old" | tee -a ${TMP_FILE}
+
+  if (( ${ELAPSED_MIN} > ${THRESHOLD} ))
   then
-    echo "Something wrong in data acquisition from mosquitto server to DB" | tee -a ${TMP_FILE}
+    PASSED=false
+  fi
+  ## Test measures
+
+  MY_TIME=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} 'sudo su postgres -c "psql -t -A -d airq -c \"SELECT max(srv_ts) from measures\" " ' 2>/dev/null)
+  echo "Most recent sensor data: ${MY_TIME}" | tee -a ${TMP_FILE}
+  MY_TIME=$(echo ${MY_TIME} | sed 's/\(.*\)\.[0-9][0-9]*\(\+.*\)/\1\2/g')
+  diff_min "${MY_TIME}"
+  echo "Data is ${ELAPSED_MIN} min old" | tee -a ${TMP_FILE}
+
+  if ((${ELAPSED_MIN} > ${THRESHOLD} ))
+  then
+    PASSED=false
+  fi
+
+  ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} 'grep -E "ERROR|CRITICAL" /var/log/mosquitto-agent/mosquitto-agent.log ' 2>/dev/null > /tmp/newErrorsAirQ
+  if [ -f /tmp/oldErrorsAirQ ]
+  then
+    ERRORS="$(diff /tmp/newErrorsAirQ /tmp/oldErrorsAirQ)"
+  else
+    ERRORS="$(cat /tmp/newErrorsAirQ)"
+  fi
+  
+  mv /tmp/newErrorsAirQ /tmp/oldErrorsAirQ
+
+  if [ ! -z "${ERRORS}" ]
+  then
+    echo "Errors in /var/log/mosquitto-agent/mosquitto-agent.log: ${ERRORS}" | tee -a ${TMP_FILE}
     PASSED=false
   else
-    echo "Data acquisition from mosquitto server to DB is OK" | tee -a ${TMP_FILE}
+    echo "No errors in /var/log/mosquitto-agent/mosquitto-agent.log" | tee -a ${TMP_FILE}
   fi
-else
-  echo "mosquitto_pub is not installed, skipping mosquitto test" | tee -a ${TMP_FILE}
-  PASSED=false
-fi
 
+  LASTSENSORDATA="$(curl sensor.waag.org/lastsensordata 2>/dev/null)"
+  if [ -z "${LASTSENSORDATA}" ]
+  then
+    echo "No last sensor data (curl sensor.waag.org/lastsensordata)" | tee -a ${TMP_FILE}
+    PASSED=false
+  else
+    echo "Last sensor data (curl sensor.waag.org/lastsensordata): ${LASTSENSORDATA}" | tee -a ${TMP_FILE}
+  fi
 
-## Test traffic data
+  if ! curl airq.waag.org 2>&1 | grep bundle.js >/dev/null
+  then
+    echo "App does not respond on airq.waag.org" | tee -a ${TMP_FILE}
+    PASSED=false
+  else
+    echo "App responds on airq.waag.org" | tee -a ${TMP_FILE}
+  fi
 
-MY_TIME=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} 'sudo su postgres -c "psql -t -A -d traffic -c \"SELECT max(timestmp) from traveltime\" " ' 2>/dev/null)
-echo "Most recent traffic data: ${MY_TIME}" | tee -a ${TMP_FILE}
-diff_min "${MY_TIME}"
-echo "Data is ${ELAPSED_MIN} min old" | tee -a ${TMP_FILE}
-
-if (( ${ELAPSED_MIN} > ${THRESHOLD} ))
-then
-  PASSED=false
-fi
-## Test measures
-
-MY_TIME=$(ssh ${SSH_OPTS} -p ${SSH_PORT} -i ${MY_KEY} ${MY_USER}@${MY_HOST} 'sudo su postgres -c "psql -t -A -d airq -c \"SELECT max(srv_ts) from measures\" " ' 2>/dev/null)
-echo "Most recent sensor data: ${MY_TIME}" | tee -a ${TMP_FILE}
-MY_TIME=$(echo ${MY_TIME} | sed 's/\(.*\)\.[0-9][0-9]*\(\+.*\)/\1\2/g')
-diff_min "${MY_TIME}"
-echo "Data is ${ELAPSED_MIN} min old" | tee -a ${TMP_FILE}
-
-if ((${ELAPSED_MIN} > ${THRESHOLD} ))
-then
-  PASSED=false
 fi
 
 if [ ! "$PASSED" = "true" ]
