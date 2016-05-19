2b4b84c200d4d0a1bcbd985d366d3da4191561c6
Stefano Bocconi <stefano@waag.org>
2016-05-19 12:44:45 +0200

    Implement batch mode and several improvements

--- Statistics/sensorActivity.R
+++ Statistics/sensorActivity.R
@@ -1,138 +1,277 @@
+#!/usr/bin/env Rscript
+
 library(ggplot2)
-library(reshape2)
+#library(reshape2)
 library(data.table)
 
-writeLines("Starting program")
+###################
+# Inputs parameters
+###################
 
-system("psql -h localhost -p 9330 -U postgres -d airq  -A -F',' -c 'select * from measures where id > 100' | grep -v 'rows)' > ./all.csv")
+args = commandArgs(trailingOnly=TRUE)
 
-start <- proc.time()
+if (length(args) == 0) {
+ # execute interactively
+  if( !interactive()){
+    stop("This is not an interactive session",call.=FALSE)
+  }
+  if (exists("startDate")){
+    rm("startDate")
+  }
+  if (exists("endDate")){
+    rm("endDate")
+  }
+  interactive <- 'y'
+
+} else if (length(args)==3) {
+  # default output file
+  startDate <- args[1]
+  endDate <- args[2]
+  interactive <- args[3]
+}else{
+  stop(paste("Wrong number of arguments:",args), call.=FALSE)
+}
+
+###################
+# Constants
+###################
+
+measures <- c("rssi","temp","humidity","pm25", "pm10","no2a","no2b")
+
+
+if ( interactive == 'y' ){
+  toFile <- readline(prompt="Output to file[y/N] ? ")
+}else{
+  toFile <- 'y'  
+}
+
+if (!exists("startDate")){
+  startDate <- readline(prompt="Enter first day (ex:18-04-2016, \"\" for earliest available day) ")
+  if (startDate == ""){
+    startDate <- NA
+  }
+}
+
+if( ! is.na(startDate) ){
+  startDate <- as.POSIXct(startDate,format = "%d-%m-%Y", tz = "Europe/Amsterdam")
+}
+
+if (!exists("endDate")){
+  endDate <- readline(prompt="Enter last day (ex:18-04-2016, \"\" for most recent available day) ")
+  
+  if (endDate == ""){
+    endDate <- NA
+  }
+}
+
+if( ! is.na(endDate) ){
+  endDate <- as.POSIXct(endDate,format = "%d-%m-%Y", tz = "Europe/Amsterdam") + 60 * 60 * 24 - 1
+}
+
+stopifnot( (endDate >= startDate) || is.na(endDate)  || is.na(startDate) )
+
+
+if ( interactive == 'y' ){
+  writeLines("\nStarting program")
+  start <- proc.time()
+}
+
+whereCondition <- "AND"
+## Create WHERE condition
+if(is.na(endDate)  && is.na(startDate) ){
+  # no WHERE condition
+  whereCondition <- paste(whereCondition,"TRUE")
+}else{
+  if ( !is.na(startDate) ){
+    whereCondition <- paste(whereCondition, " srv_ts >= '",startDate,"'",sep="")
+  }
+  if ( !is.na(startDate) && !is.na(endDate) ){
+    whereCondition <- paste(whereCondition, "AND")
+  }
+  if ( !is.na(endDate) ){
+    whereCondition <- paste(whereCondition, " srv_ts <= '",endDate,"'",sep="")
+  }
+}
+
+
+## Read data from tunnel to server
+query <- paste("select * from measures where id > 100",whereCondition)
+command <- paste("psql -h localhost -p 9330 -U postgres -d airq  -A -F',' -c \"", query, "\" | grep -v 'rows)' > ./all.csv", sep="")
+
+system(command)
+
 
 all <- read.csv("./all.csv", header=TRUE, stringsAsFactors = FALSE)
 
-writeLines("Done reading data file")
-writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
-start <- proc.time()
+if ( interactive == 'y' ){
+  writeLines("\nDone reading data file")
+  writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
+}
 
+## Adapt date format
 
 all$tr_ts <- paste(gsub("\\.[0-9]*","",all$srv_ts),"00",sep="")
-
 all$corr_ts <- as.POSIXct(all$tr_ts, format = "%Y-%m-%d %H:%M:%S%z")
-
-#all$corr_ts <- as.POSIXct(paste(all$srv_ts,"00",sep=""), format = "%Y-%m-%d %H:%M:%S.%OS%z")
-
-fout <- all[is.na(all$corr_ts),c("srv_ts","tr_ts","corr_ts")]
-
 stopifnot(sum(is.na(all$corr_ts))== 0)
 
+## uniform empty messages
+all$message[is.na(all$message) | all$message==""] <- NA
+
+#all$corr_ts <- as.POSIXct(paste(all$srv_ts,"00",sep=""), format = "%Y-%m-%d %H:%M:%S.%OS%z")
+#fout <- all[is.na(all$corr_ts),c("srv_ts","tr_ts","corr_ts")]
 #all$corr_ts[is.na(all$corr_ts)] <- as.POSIXct("01-01-2000 13:00:00",format = "%d-%m-%Y %H:%M:%S", tz = "Europe/Amsterdam")
+
+
+## initialize start and end dates
 
 #startDate <- as.POSIXct("14-04-2016 13:00:00",format = "%d-%m-%Y %H:%M:%S", tz = "Europe/Amsterdam")
 #endDate <- as.POSIXct("18-04-2016 13:00:00",format = "%d-%m-%Y %H:%M:%S", tz = "Europe/Amsterdam")
 
 startDate <- trunc(min(all$corr_ts), units = "days")
-endDate <- trunc(max(all$corr_ts), units = "days") + 60 * 60 * 24
+endDate <- trunc(max(all$corr_ts), units = "days") + 60 * 60 * 24 -1
 
-n <- readline(prompt="Enter first day (ex:18-04-2016, \"\" for earliest available day) ")
-
-if (n == ""){
-
-}else{
-  startDate <- as.POSIXct(n,format = "%d-%m-%Y", tz = "Europe/Amsterdam")
+if ( interactive == 'y' ){
+  start <- proc.time()
 }
 
-n <- readline(prompt="Enter last day (ex:18-04-2016, \"\" for most recent available day) ")
+## reduce dataset to time interval
 
-if (n == ""){
-  
-}else{
-  endDate <- as.POSIXct(n,format = "%d-%m-%Y", tz = "Europe/Amsterdam") + 60 * 60 * 24 - 1
+sensorData <- data.table(all[c("id","corr_ts","message", measures)],key="id")
+sensorData <- sensorData[corr_ts>=startDate & corr_ts<=endDate,]
+sensorData$id <-as.factor(sensorData$id)
+
+idsInRange <- sensorData[,id,by = id]$id
+
+if ( interactive == 'y' ){
+  writeLines("\nAvailable sensor ids:")
+  print(idsInRange)
 }
-
-stopifnot( endDate >= startDate)
 
 hours <- floor(difftime(endDate,startDate,units="hours")) + 1
 
-bins <- data.frame(tm=vector(mode="character", length=3*hours), data=vector(mode="integer", length=3*hours), type=vector(mode="character", length=3*hours),
+datatypes <- c("Full data", "Partial data", "Startup")
+
+len <- length(datatypes) * as.integer(hours) * nlevels(idsInRange)
+
+bins <- data.frame(tm=.POSIXct(character(len)),
+                   id=integer(len),
+                   datapoints=integer(len),
+                   datatype=character(len),
                    stringsAsFactors = FALSE)
 
-writeLines(paste("Done calculating time frame, hours: ",hours))
-writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
-start <- proc.time()
+if ( interactive == 'y' ){
+  writeLines(paste("\nDone calculating time frame, hours: ",hours))
+  writeLines(sprintf("Time interval for plotting from %s to %s",
+                     as.character(startDate,format = "%d-%m-%Y %H:%M:%S", tz = "Europe/Amsterdam"),
+                     as.character(endDate,format = "%d-%m-%Y %H:%M:%S", tz = "Europe/Amsterdam")))
+  writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
+  start <- proc.time()
+}
 
 timeNow <- startDate
 index <- 1
 
-s_data <- ( all$message == "" & ! is.na(all$temp) )
-s_emptydata <- ( all$message == "" &  is.na(all$temp) )
-s_startup <- ( all$message != "" )
 
-stopifnot( sum(s_data) + sum (s_emptydata) + sum(s_startup) == nrow(all))
+s_fulldata <- (rowSums(is.na(sensorData[,measures,with=FALSE])) == 0)
+s_partialdata <- (is.na(sensorData$message) & rowSums(is.na(sensorData[,measures,with=FALSE])) > 0)
+s_startup <- (!is.na(sensorData$message))
 
-while (index < 3*hours)
+stopifnot( sum(s_fulldata) + sum (s_partialdata) + sum(s_startup) == nrow(sensorData))
+
+while (index <= (len-(length(datatypes)*nlevels(idsInRange)) + 1))
 {
-  candidates <- all$corr_ts<=timeNow & all$corr_ts > (timeNow-3600)
+  candidates <- sensorData$corr_ts<(timeNow+3600) & sensorData$corr_ts >= timeNow
   
-  bins$data[index] <- sum( candidates & s_data )
-  bins$data[index+1] <- sum( candidates & s_emptydata )
-  bins$data[index+2] <- sum( candidates & s_startup )
-  
-  bins$type[index] <- "data"
-  bins$type[index+1] <- "empty data"
-  bins$type[index+2] <- "startup"
-  
-#  if (timeNow == trunc(timeNow, units = "days"))
-#  {
-    time_s <- strftime(timeNow,format="%d:%m %H")
-    bins$tm[index] <- time_s
-    bins$tm[index+1] <- time_s
-    bins$tm[index+2] <- time_s
-#  }else{
-#    bins$tm[index] <- strftime(timeNow,format="%H")
-#    bins$tm[index+1] <- strftime(timeNow,format="%H")
-#  }
+  for ( id_index in 1: nlevels(idsInRange) )
+  {
+    currentID <- levels(idsInRange)[id_index]
+    idcandidates <- candidates & (sensorData$id == currentID)
+
+    bins$datapoints[index] <- sum( idcandidates & s_fulldata )
+    bins$datapoints[index+1] <- sum( idcandidates & s_partialdata )
+    bins$datapoints[index+2] <- sum( idcandidates & s_startup )
+    
+
+    for ( inner_index in 0:(length(datatypes)-1) )
+    {
+      bins$datatype[index+inner_index] <- datatypes[inner_index+1]
+      bins$tm[index+inner_index] <- timeNow
+      bins$id[index+inner_index] <- currentID
+    }
+    index <- index + 3  
+  }
   
   timeNow <- timeNow + 3600
-  index <- index + 3
+  
 }
 
-writeLines("Done calculating graph data")
-writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
-start <- proc.time()
+if ( interactive == 'y' ){
+  writeLines("\nDone calculating graph data")
+  writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
+  start <- proc.time()
+}
 
-pl <- ggplot(bins, aes(x=tm,y=data, fill=factor(type)) ) + geom_bar(position="dodge",stat="identity") +
+if ( toFile == 'y' ){
+  title <- paste("Report at ", Sys.time(),  sprintf(" from %s to %s",
+                                                    as.character(startDate,format = "%d-%m-%Y", tz = "Europe/Amsterdam"),
+                                                    as.character(endDate,format = "%d-%m-%Y", tz = "Europe/Amsterdam")),".pdf", sep="")
+  title <- gsub(":","_",title)
+  dir <- "/Users/SB/Downloads/"
+  pdf(file=paste(dir,title,sep=""),title=title,paper="a4r",width=14)
+}
+
+
+for (i in 1:length(datatypes))
+{
+  
+  if ( toFile != 'y' ){
+    readline(prompt=paste("Press enter to see ",datatypes[i],sep=""))
+  }
+  
+  currentBin <- bins[bins$datatype == datatypes[i],]
+  
+  pl <- ggplot(data=currentBin, aes(x=tm, y=datapoints, group=id, colour=id)) + 
+    geom_line() +
+    xlab("Time") +
+    ylab(paste("Nr of",datatypes[i],"sensor msg")) +
+    theme(axis.text.x = element_text(angle = -90, hjust = 1))
+  
+  print(pl)
+  
+}
+#pl <- ggplot(bins, aes(x=tm,y=datapoints, fill=factor(datatype)) ) + geom_bar(position="dodge",stat="identity") +
 #  scale_fill_discrete(name="Type of msg", labels=c("Data", "Startup")) +
 #  scale_fill_discrete(name="Restart") +
 #  scale_color_discrete("Restart") +
-  xlab("Time") +
-  ylab(sprintf("Nr of sensor msg from %s to %s", startDate,endDate)) +
-  theme(axis.text.x = element_text(angle = -90, hjust = 1))
-
-print(pl)
-
-measures <- c("rssi","temp","humidity","pm25", "pm10","no2a","no2b")
-
-sensorData <- data.table(all[c("id","corr_ts",measures)],key="id")
-
-sensorData <- sensorData[corr_ts>=startDate & corr_ts<=endDate,]
-
-sensorData$id <-as.factor(sensorData$id)
-
-
-writeLines("Available sensor ids: ")
-print(sensorData[,id,by = id]$id)
 
 #n <- readline(prompt="Enter sensor number: ")
 #n <- as.integer(n)
 
+normalFactor <- 100
+validData <- sensorData[!s_startup,]
 
 for (i in 1:length(measures))
 {
-  readline(prompt=paste("Press enter to see ",measures[i],sep=""))
+  maat <- median(validData[,measures[i],with=FALSE][[1]])
   
-  pl <- ggplot(data=sensorData, aes_string(x="corr_ts", y=measures[i], group="id", colour="id")) + 
-    geom_line() 
-
+#  inData <- validData[abs(get(measures[i])) <= abs(maat*normalFactor),]
+  if ( toFile != 'y' ){
+    readline(prompt=paste("Press enter to see ",measures[i],sep=""))
+  }
+  
+  pl <- ggplot(data=validData, aes_string(x="corr_ts", y=measures[i], group="id", colour="id")) + 
+    geom_line() +
+    xlab("Time") +
+    theme(axis.text.x = element_text(angle = -90, hjust = 1))
+    
   print(pl)
-  
+}
+
+if ( toFile == 'y' ){
+  garbage <- dev.off()
+}
+
+if ( interactive == 'y' ){
+  writeLines("\nProgram done")
+  writeLines(paste("Elapsed secs:", (proc.time()-start)["elapsed"],"\n"))
 }
