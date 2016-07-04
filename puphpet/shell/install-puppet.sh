8f771736a6e5313a5bf4a24a8ccca0ce2c81ac0c
Stefano Bocconi <stefano@waag.org>
2016-07-04 11:55:24 +0200

    Fix for active support broken dependency

--- puphpet/shell/install-puppet.sh
+++ puphpet/shell/install-puppet.sh
@@ -31,5 +31,8 @@ if [[ ! -f /.puphpet-stuff/install-puppet ]]; then
     touch /.puphpet-stuff/install-puppet
 fi
 
-/opt/puppetlabs/puppet/bin/gem install deep_merge activesupport vine --no-ri --no-rdoc
+#/opt/puppetlabs/puppet/bin/gem install deep_merge activesupport vine --no-ri --no-rdoc
 #/opt/puppetlabs/puppet/bin/gem install deep_merge vine --no-ri --no-rdoc
+/opt/puppetlabs/puppet/bin/gem install deep_merge -v 1.0.1 --no-ri --no-rdoc
+/opt/puppetlabs/puppet/bin/gem install activesupport -v 4.2.6 --no-ri --no-rdoc
+/opt/puppetlabs/puppet/bin/gem install vine -v 0.2 --no-ri --no-rdoc
